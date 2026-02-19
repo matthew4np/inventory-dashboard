8e1f4116019be59609ccf29b5a0a82f8a0073d7d
Ding Yoke Ong <ICT_6924812P@connect.np.edu.sg>
2026-02-19 20:00:32 +0800

    H3

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm dev',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://localhost:3000/dashboard',
     reuseExistingServer: !process.env.CI,
     stdout: 'ignore',
     stderr: 'pipe',
