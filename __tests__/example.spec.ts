13d557821e4e30ff781fed832adb2ccaad9eb4c5
Ding Yoke Ong <ICT_6924812P@connect.np.edu.sg>
2026-02-19 20:17:58 +0800

    H4

--- __tests__/example.spec.ts
+++ __tests__/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('Reach Loan Page successfully', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000/dashboard');
+  await page.goto('http://localhost:3000/dashboard');
 
   // Expect a title "to contain" a substring.
   await expect(page.getByText('Loan', {exact: true})).toBeVisible;
 });
 
 test('Create Loan link', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000/dashboard/loans2/');
+  await page.goto('http://localhost:3000/dashboard/loans2/');
 
   // Click the get started link.
   await page.getByText('Create Loan', { exact: true}).click();
